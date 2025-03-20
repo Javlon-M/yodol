790ee589424946a6288bbc6be7d4aff9c10bfe2e
Mirzaabdullaev Javlon <mirzaabdullayevtest@gmail.com>
2025-03-20 19:07:46 +0500

    refactor: card-factory injection has been implemented as singleton

--- src/factories/dependency-module.ts
+++ src/factories/dependency-module.ts
@@ -5,5 +5,6 @@ import * as Factories from "../factories/"
 import { FactorySymbols } from "./dependency-symbols"
 
 export const FactoryContainerModule = new ContainerModule((bind: interfaces.Bind) => {
-    bind<Factories.CardFactory>(FactorySymbols.CardFactory).to(Factories.CardFactoryImpl)
+    bind<Factories.CardFactory>(FactorySymbols.CardFactory)
+    .to(Factories.CardFactoryImpl).inSingletonScope()
 })

1b69c9fba04c9c0defc3333de8f4eaad7f4a3064
bakhromkhuja <aglamkhujaev@gmail.com>
2025-03-25 18:07:52 +0500

    refactor: remove base interface file

--- src/use-cases/base-interface.ts
+++
@@ -1,8 +0,0 @@
-export interface BaseInterfaceForUseCases<Arguments, ReturnType> {
-    execute(params: Arguments): Promise<ReturnType | ResultOfUseCaseOnError>;
-}
-  
-export interface ResultOfUseCaseOnError {
-    error: boolean;
-    message: string;
-}

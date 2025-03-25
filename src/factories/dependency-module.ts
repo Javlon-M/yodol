8a8b17b7f869f7c94170786122801ca0bfcb1bf5
Mirzaabdullaev Javlon <mirzaabdullayevtest@gmail.com>
2025-03-25 20:00:28 +0500

    refactor: readiblity changes

--- src/factories/dependency-module.ts
+++ src/factories/dependency-module.ts
@@ -5,12 +5,15 @@ import * as Factories from "../factories/"
 import { FactorySymbols } from "./dependency-symbols"
 
 export const FactoryContainerModule = new ContainerModule((bind: interfaces.Bind) => {
+    // Card
     bind<Factories.CardFactory>(FactorySymbols.CardFactory)
     .to(Factories.CardFactoryImpl).inSingletonScope()
 
+    // Deck
     bind<Factories.DeckFactory>(FactorySymbols.DeckFactory)
     .to(Factories.DeckFactoryImpl).inSingletonScope()
 
+    // Identifier
     bind<Factories.IdentifierFactory>(FactorySymbols.IdentifierFactory)
     .to(Factories.IdentifierFactoryImpl).inSingletonScope()
 })
--- src/repositories/dependency-module.ts
+++ src/repositories/dependency-module.ts
@@ -6,9 +6,11 @@ import { RepositorySymbols } from "./dependency-symbols"
 
 
 export const RepositoryContainerModule = new ContainerModule((bind: interfaces.Bind) => {
+    // Card
     bind<Repositories.CardRepository>(RepositorySymbols.CardRepository)
     .to(Repositories.CardRepositoryImpl).inSingletonScope()
 
+    // Deck
     bind<Repositories.DeckRepository>(RepositorySymbols.DeckRepository)
     .to(Repositories.DeckRepositoryImpl).inSingletonScope()
 })

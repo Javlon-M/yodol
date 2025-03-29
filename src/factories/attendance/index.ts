3b24d7329f2f28155c98e2ec2e682d82cc1553fb
Mirzaabdullaev Javlon <mirzaabdullayevtest@gmail.com>
2025-03-30 03:06:12 +0500

    feat: attendance factory has been created

---
+++ src/factories/attendance/index.ts
@@ -0,0 +1,27 @@
+import * as Inversify from "inversify"
+
+import * as Domain from "app/domain"
+
+
+export interface AttendanceFactory {
+    construct(params: Params): Domain.Attendance
+}
+
+@Inversify.injectable()
+export class AttendanceFactoryImpl implements AttendanceFactory {
+    public construct(params: Params): Domain.Attendance {
+        return new Domain.Attendance(
+            params.id,
+            params.month,
+            params.userId,
+            params.attended
+        )
+    }
+}
+
+interface Params {
+    id: Domain.Identifier
+    month: string
+    userId: string
+    attended: number[]
+}

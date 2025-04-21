e70d07bf6c94d25dacd3096808dd5838157677a6
Mirzaabdullaev Javlon <mirzaabdullayevtest@gmail.com>
2025-04-21 20:11:58 +0500

    refactor: typing changes in deck remove use case

--- src/use-cases/deck/remove/index.ts
+++ src/use-cases/deck/remove/index.ts
@@ -1,7 +1,9 @@
 import * as Inversify from "inversify"
+import * as Factories from "app/factories"
 import * as Repositories from "app/repositories"
 
 import { RepositorySymbols } from "app/repositories/dependency-symbols"
+import { FactorySymbols } from "app/factories/dependency-symbols"
 
 export interface RemoveDeckUseCase {
   remove(params: Params): Promise<Response>
@@ -11,10 +13,11 @@ export interface RemoveDeckUseCase {
 export class RemoveDeckUseCaseImpl implements RemoveDeckUseCase {
   constructor(
     @Inversify.inject(RepositorySymbols.DeckRepository) private deckRepository: Repositories.DeckRepository,
+    @Inversify.inject(FactorySymbols.IdentifierFactory) private identifierFactory: Factories.IdentifierFactory,
     ) {}
 
   public async remove(params: Params): Promise<Response> {
-    const deck = await this.deckRepository.remove({id: params.deckId})
+    const deck = await this.deckRepository.remove({id: this.identifierFactory.construct(params.deckId)})
 
     if (!deck) {
       throw new Error('Deck not found')

df16daf2a0fc8a889c6a610984a5e4dffbf688e1
Mirzaabdullaev Javlon <mirzaabdullayevtest@gmail.com>
2025-05-22 10:55:52 +0500

    feat: answering lrn card flow has implemented

--- src/use-cases/card/answer/index.ts
+++ src/use-cases/card/answer/index.ts
@@ -49,11 +49,11 @@ export class AnswerCardUseCaseImpl implements AnswerCardUseCase {
         }
     }
 
-    private async answerNewCard(card: Domain.Card, deck: Domain.Deck) {
+    private async answerNewCard(card: Domain.Card, deck: Domain.Deck): Promise<Domain.Card> {
         const reps = card.getRepetitions() + 1
         const conf = this.getConf(card, deck)
 
-        await this.cardRepository.update({
+        return await this.cardRepository.update({
             id: card.getId(),
             repetitions: reps,
             queue: 1,
@@ -62,42 +62,138 @@ export class AnswerCardUseCaseImpl implements AnswerCardUseCase {
         })
     }
 
-    private async answerLrnCard(card: Domain.Card, deck: Domain.Deck, ease: number) {
+    private async answerLrnCard(card: Domain.Card, deck: Domain.Deck, ease: number): Promise<Domain.Card> {
         const conf = this.getConf(card, deck)
 
         if (ease === 4) {
-            await this.reschaduleAsRev(card, conf, true)
+            return await this.rescheduleAsRev(card, conf, true)
         }
         else if (ease === 3) {
             if (card.getLeft() - 1 <= 0) {
-                await this.reschaduleAsRev(card, conf, true)
+                return await this.rescheduleAsRev(card, conf, false)
             }
             else {
-                await this.moveToNextStep(card, conf)
+                return await this.moveToNextStep(card, conf)
             }
         }
         else if (ease == 2) {
-            await this.repeatStep(card, conf)
+            return await this.repeatStep(card, conf)
         }
         else {
-            await this.moveToFirstStep(card, conf)
+            return await this.moveToFirstStep(card, conf)
         }
     }
 
-    private async reschaduleAsRev(card: Domain.Card, conf: Conf, early: boolean) {
+    private async rescheduleAsRev(card: Domain.Card, conf: Conf, early: boolean): Promise<Domain.Card> {
+        const lapse = card.getType() === Domain.CardTypes.Review
 
+        if (lapse) {
+            return await this.rescheduleGraduatingLapse(card)
+        }
+        else {  
+            return await this.rescheduleNew(card, conf as ConfNew, early)
+        }
     }
 
-    private async moveToNextStep(card: Domain.Card, conf: Conf) {
+    private async rescheduleGraduatingLapse(card: Domain.Card): Promise<Domain.Card> {
+        const today = new Date()
+        today.setDate(today.getDate() + card.getInterval())
 
+        const due = today.getTime()
+
+        return await this.cardRepository.update({
+            id: card.getId(),
+            due,
+            type: Domain.CardTypes.Review,
+            queue: Domain.CardQueues.rev
+        })
     }
 
-    private async repeatStep(card: Domain.Card, conf: Conf) {
+    private async rescheduleNew(card: Domain.Card, conf: ConfNew, early: boolean): Promise<Domain.Card> {
+        const ivl = this.graduatingInv(card, conf, early)
 
+        const today = new Date()
+        today.setDate(today.getDate() + ivl)
+
+        const due = today.getTime()
+        const factor = conf.initialFactor
+
+        return await this.cardRepository.update({
+            id: card.getId(),
+            interval: ivl,
+            due,
+            factor,
+            type: Domain.CardTypes.Review,
+            queue: Domain.CardQueues.rev
+        })
     }
 
-    private async moveToFirstStep(card: Domain.Card, conf: Conf) {
+    private graduatingInv(card: Domain.Card, conf: ConfNew, early: boolean): number {
+        if ([Domain.CardTypes.Relearning, Domain.CardTypes.Review].includes(card.getType())) {
+            return card.getInterval()
+        }
 
+        if (early) {
+            return conf.ints[1]
+        }
+
+        return conf.ints[0]
+    }
+
+    private async moveToNextStep(card: Domain.Card, conf: Conf): Promise<Domain.Card> {
+        const left = card.getLeft() - 1
+        const delay = conf.delays[-left] * 60
+
+        const updatedCard = await this.cardRepository.update({
+            id: card.getId(),
+            left
+        })
+
+        return await this.rescheduleLrnCard(updatedCard, conf, delay)
+    }
+
+    private async repeatStep(card: Domain.Card, conf: Conf): Promise<Domain.Card> {
+        const delay1 = conf.delays[-card.getLeft()] * 60
+        const delay2 = conf.delays[-card.getLeft() - 1] * 60
+        const avg = Math.floor((delay1 + Math.max(delay1, delay2)) / 2)
+
+        return await this.rescheduleLrnCard(card, conf, avg)
+    }
+
+    private async moveToFirstStep(card: Domain.Card, conf: Conf): Promise<Domain.Card> {
+        const left = conf.delays.length
+        const updatedCard = await this.cardRepository.update({
+            id: card.getId(),
+            left
+        })
+
+        if (updatedCard.getType() == Domain.CardTypes.Relearning) {
+            await this.updateRevIvlOnFail(updatedCard, conf as ConfLapse)
+        }
+
+        return this.rescheduleLrnCard(updatedCard, conf)
+    }
+
+    private async updateRevIvlOnFail(card: Domain.Card, conf: ConfLapse): Promise<Domain.Card> {
+        const ivl = Math.max(1, conf.minInt, card.getInterval() * conf.mult)
+
+        return await this.cardRepository.update({
+            id: card.getId(),
+            interval: ivl
+        })
+    }
+
+    private async rescheduleLrnCard(card: Domain.Card, conf: Conf, delay?: number): Promise<Domain.Card> {
+        if (!delay) {
+            delay = conf.delays[-card.getLeft()] * 60
+        }
+        const due = (new Date()).getTime() + delay
+        
+        return await this.cardRepository.update({
+            id: card.getId(),
+            due,
+            queue: Domain.CardQueues.lrn
+        })
     }
 
     private async answerRevCard(card: Domain.Card, deck: Domain.Deck) {
